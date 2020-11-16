e008e3cdd04e435f28ded922202548b3d26be579
Marcin Majewski <marcin.majewski@sonarsource.com>
2020-11-16 10:30:00 +0100

    unbound-test

--- new_file.js
+++ new_file.js
@@ -58,6 +58,4 @@ var merged = arr.reduce(function(a, b) {
   a.concat(b);
 });var merged = arr.reduce(function(a, b) {
   a.concat(b);
-});var merged = arr.reduce(function(a, b) {
-  a.concat(b);
 });
