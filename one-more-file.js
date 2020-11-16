8fc2c80d216fc257fbe56d46c4d037921258cac2
Marcin Majewski <marcin.majewski@sonarsource.com>
2020-11-16 10:44:20 +0100

    another testing commit

---
+++ one-more-file.js
@@ -0,0 +1,48 @@
+function* myGen(a, b) {  // Noncompliant
+  let answer = 0;
+  answer += a * b;
+}
+function* myGen(a, b) {  // Noncompliant
+  let answer = 0;
+  answer += a * b;
+}
+function* myGen(a, b) {  // Noncompliant
+  let answer = 0;
+  answer += a * b;
+}
+function* myGen(a, b) {  // Noncompliant
+  let answer = 0;
+  answer += a * b;
+}
+function* myGen(a, b) {  // Noncompliant
+  let answer = 0;
+  answer += a * b;
+}
+function* myGen(a, b) {  // Noncompliant
+  let answer = 0;
+  answer += a * b;
+}
+function* myGen(a, b) {  // Noncompliant
+  let answer = 0;
+  answer += a * b;
+}
+function* myGen(a, b) {  // Noncompliant
+  let answer = 0;
+  answer += a * b;
+}
+function* myGen(a, b) {  // Noncompliant
+  let answer = 0;
+  answer += a * b;
+}
+function* myGen(a, b) {  // Noncompliant
+  let answer = 0;
+  answer += a * b;
+}
+function* myGen(a, b) {  // Noncompliant
+  let answer = 0;
+  answer += a * b;
+}
+function* myGen(a, b) {  // Noncompliant
+  let answer = 0;
+  answer += a * b;
+}
