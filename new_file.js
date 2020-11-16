963bc2abbce62940bfe2bc92c1d13bbe01ef4a77
Marcin Majewski <marcin.majewski@sonarsource.com>
2020-11-16 10:26:37 +0100

    bugs

---
+++ new_file.js
@@ -0,0 +1,63 @@
+var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});
