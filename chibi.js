f9bec151db792769811e1fa1debd02ae391d8011
Marcin Majewski <marcin.majewski@sonarsource.com>
2020-11-19 16:14:27 +0100

    Chibi

--- chibi.js
+++ chibi.js
@@ -3,7 +3,7 @@
 //	'use strict';
 
 	var readyfn = [],
-		loadedfn = [2, 3, 4, 5, 6, 7, 8, 9, 10],
+		loadedfn = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
 		domready = true,
 		pageloaded = false,
 		jsonpcount = 0,
